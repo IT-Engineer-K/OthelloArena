7045ba721286a38e0597fb668669cf62c13d9bcc
unknown <funaharakoju@gmail.com>
2024-11-21 12:39:46 +0900

    最終版

--- OthelloAction.py
+++ OthelloAction.py
@@ -1,5 +1,6 @@
 import OthelloLogic
 import Action
+
 class ActionClass:
     def __init__(self, evaluate_func):
         self.evaluate = evaluate_func
@@ -37,19 +38,5 @@ class ActionClass:
             return min_eval
 
     # 次の手を決定する関数
-    def getAction(self, board, moves, depth=3):
-        return Action.getAction(board, moves)
-        max_score = -float('inf')
-        best_move = moves[0]
-
-        # 各手について評価し、最適な手を選択
-        for move in moves:
-            next_board = OthelloLogic.execute(OthelloLogic.copy(board), move, 1, 8)
-
-            # 深さ7まで読み込んでスコアを計算
-            score = self.alphaBetaMinimax(next_board, depth, -float('inf'), float('inf'), False)
-            if score > max_score:
-                max_score = score
-                best_move = move
-
-        return best_move
+    def getAction(self, board, moves, depth=6):
+        return Action.getAction(board, moves, depth=6)
