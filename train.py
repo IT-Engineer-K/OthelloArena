0cdc7fc48306a17d35648ce532dd6feaf7f7655b
unknown <funaharakoju@gmail.com>
2024-11-26 19:16:03 +0900

    a

--- train.py
+++ train.py
@@ -7,9 +7,6 @@ print(res.status_code)
 if res.status_code == 200:
     with open("data.pickle", "wb") as f:
         f.write(res.content)
-if os.path.exists("data.pickle"):
-    with open("data.pickle", "rb") as f:
-        print(requests.post("https://aaaaaaaaaaaaaa.pythonanywhere.com/upload", files={"file": f}).text)
 
 for i in range(10):
     subprocess.run(["python", "tournament_loop.py"])
